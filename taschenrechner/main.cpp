d29054fcf423b36c2f11852a6de5a07dff8a234d
Cem-Kaplan <cemk46908@gmail.com>
2026-02-21 19:26:10 +0100

    c++ taschenrechner angefangen

---
+++ taschenrechner/main.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+std::string input;
+float calculate(std::string x);
+bool checkCalculatable(std::string x);
+
+int main() {
+
+    while (true) {
+
+        std::cout << "Rechnung eingeben\n"
+                  << "+ -> Plus\n"
+                  << "- -> Minus\n"
+                  << "/ -> Geteilt\n"
+                  << "* -> Mal\n";
+
+        std::cin >> input;
+        if (input.length() == 0) break;
+        else if (input.length() > 0) calculate(input);
+    }
+    return 0;
+}
+
+float calculate(std::string x) {
+    if (checkCalculatable(x)) {
+        
+    } else {
+    }
+}
+
+bool checkCalculatable(std::string x) {
+    bool canCalculate = true;
+    for (int i = 0; i < x.length(); i++) {
+        if (x.at(i) == '+') {
+            std::cout << "Ein Plus gefunden\n";
+        } else if (x.at(i) == '-') {
+            std::cout << "Ein Minus gefunden\n";
+        } else if (x.at(i) == '*') {
+            std::cout << "Ein Stern gefunden\n";
+        }else if (x.at(i) == '/') {
+            std::cout << "Ein Slash gefunden\n";
+        } else {
+            canCalculate = false;
+            std::cerr << "Kein Rechenoperator";
+        }
+    }
+    return canCalculate;
+}
